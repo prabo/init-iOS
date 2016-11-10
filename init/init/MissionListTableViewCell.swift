b07cbe4258f39e818cf4cbf77c294f8c025decc6
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2016-11-10 20:51:36 +0900

    missionListのtableview作成

---
+++ init/init/MissionListTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MissionListTableViewCell.swift
+//  init
+//
+//  Created by Atsuo on 2016/11/10.
+//  Copyright © 2016年 Atsuo. All rights reserved.
+//
+
+import UIKit
+
+class MissionListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var missionNameLabel: UILabel!
+    @IBOutlet weak var missionCompleatedStatus: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
