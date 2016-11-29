68bedc09873696de0938147755dc0dd8575bcea1
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2016-11-29 13:39:26 +0900

    missionListのJSONトライ

---
+++ init/init/UserDefaultsHelper.swift
@@ -0,0 +1,18 @@
+//
+//  UserDafaultsHelper.swift
+//  init
+//
+//  Created by Atsuo on 2016/11/29.
+//  Copyright © 2016年 Atsuo. All rights reserved.
+//
+
+import UIKit
+
+class UserDafaultsHelper{
+    static func isLogin() -> Bool {
+        let ud = UserDefaults.init()
+        return ud.string(forKey: "access_token") != nil
+    }
+
+}
+
