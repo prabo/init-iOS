0d7cc94cc68410fa3084e612d061a7b78171d246
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-12-05 20:37:07 +0900

    hoge

--- init/init/Misson.swift
+++ init/init/Misson.swift
@@ -4,6 +4,7 @@
 //	Create by Atsuo on 29/11/2016
 //	Copyright © 2016. All rights reserved.
 //	Model file generated using JSONExport: https://github.com/Ahmed-Ali/JSONExport
+// hoge
 
 import Foundation
 import SwiftyJSON
