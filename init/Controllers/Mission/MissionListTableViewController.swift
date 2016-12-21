f9f89d794904439d84608475aea22923947ab5e1
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-12-21 22:16:38 +0900

    improve method order

--- init/Controllers/Mission/MissionListTableViewController.swift
+++ init/Controllers/Mission/MissionListTableViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Atsuo on 2016/11/10.
 //  Copyright © 2016年 Atsuo. All rights reserved.
 //
-//
+
 import UIKit
 import Alamofire
 import SwiftyJSON
@@ -15,6 +15,19 @@ final class MissionListTableViewController: UITableViewController {
     var missions: [Mission] = []
     var incompletedMissions: [Mission] = []
     var showOnlyIncompleted = false
+
+    @IBAction func addButton(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "MissionAddController", bundle: nil)
+        let missionAddController = storyboard.instantiateInitialViewController()
+        guard let secondViewController = missionAddController as? MissionAddController else {
+            return
+        }
+        navigationController?.pushViewController(secondViewController, animated: true)
+    }
+
+    @IBAction func reloadButton(_ sender: UIButton) {
+        getMissionLists()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,6 +58,47 @@ final class MissionListTableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
     }
 
+    func getMissionLists() {
+
+        let headers: HTTPHeaders = [
+            "Authorization":UserDefaultsHelper.getToken(),
+            "Accept": "application/json"
+        ]
+
+        Alamofire.request("https://init-api.elzup.com/v1/missions", headers:headers)
+            .responseJSON { response in
+                guard let object = response.result.value else {
+                    return
+                }
+                let json = JSON(object)
+                self.missions.removeAll()
+                json.forEach { (_, json) in
+                    self.missions.append(Mission(json: json))
+                }
+                self.tableView.reloadData()
+        }
+    }
+
+    func toggleFilter() {
+        createIncompletedMissionsArray()
+        showOnlyIncompleted = showOnlyIncompleted ? false : true
+        tableView.reloadData()
+    }
+
+    private func createIncompletedMissionsArray() {
+        incompletedMissions = []
+        missions.forEach({
+            if !$0.isCompleted {
+                incompletedMissions.append($0)
+            }
+        })
+    }
+
+}
+
+// MARK: UITableViewDataSource method
+
+extension MissionListTableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -67,7 +121,7 @@ final class MissionListTableViewController: UITableViewController {
         missionCell.ownerImage.image = UIImage(named: "enemy.png")
         let isCompleted = array[indexPath.row].isCompleted
         if isCompleted {
-        missionCell.checkImage.isHidden = false
+            missionCell.checkImage.isHidden = false
         } else {
             missionCell.checkImage.isHidden = true
         }
@@ -83,6 +137,7 @@ final class MissionListTableViewController: UITableViewController {
         }
         return missionCell
     }
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let storyboard = UIStoryboard(name: "MissionDetailController", bundle: nil)
         let missionDetailController = storyboard.instantiateInitialViewController()
@@ -94,54 +149,6 @@ final class MissionListTableViewController: UITableViewController {
         secondViewController.title = "詳細"
         secondViewController.mission = mission
         navigationController?.pushViewController(secondViewController, animated: true)
-    }
-
-    func getMissionLists() {
-
-        let headers: HTTPHeaders = [
-            "Authorization":UserDefaultsHelper.getToken(),
-            "Accept": "application/json"
-        ]
-
-        Alamofire.request("https://init-api.elzup.com/v1/missions", headers:headers)
-            .responseJSON { response in
-                guard let object = response.result.value else {
-                    return
-                }
-                let json = JSON(object)
-                self.missions.removeAll()
-                json.forEach { (_, json) in
-                    self.missions.append(Mission(json: json))
-                }
-                self.tableView.reloadData()
-        }
-    }
-
-    @IBAction func addButton(_ sender: UIButton) {
-        let storyboard = UIStoryboard(name: "MissionAddController", bundle: nil)
-        let missionAddController = storyboard.instantiateInitialViewController()
-        guard let secondViewController = missionAddController as? MissionAddController else {
-            return
-        }
-        navigationController?.pushViewController(secondViewController, animated: true)
-   }
-    @IBAction func reloadButton(_ sender: UIButton) {
-        getMissionLists()
-    }
-
-    func toggleFilter() {
-        createIncompletedMissionsArray()
-        showOnlyIncompleted = showOnlyIncompleted ? false : true
-        tableView.reloadData()
-    }
-
-    private func createIncompletedMissionsArray() {
-        incompletedMissions = []
-        missions.forEach({
-            if !$0.isCompleted {
-                incompletedMissions.append($0)
-            }
-        })
     }
 
 }
