059e18ab47023f090b0989f490f72a1977be43dc
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2016-12-30 17:19:23 +0900

    categoryListの基本機能を記述した

--- init/Controllers/Mission/MissionCategoryTableViewController.swift
+++ init/Controllers/Mission/MissionCategoryTableViewController.swift
@@ -7,89 +7,83 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class MissionCategoryTableViewController: UITableViewController {
+final class MissionCategoryTableViewController: UITableViewController {
+    var categorys : [JSON] = []
+    
+    @IBAction func addButton(_ sender: UIButton) {
+    }
+    @IBAction func reroadButton(_ sender: UIButton) {
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        getCategoryLists()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        getCategoryLists()
+    }
+    
+    func getCategoryLists() {
+        
+        let headers: HTTPHeaders = [
+            "Authorization":UserDefaultsHelper.getToken(),
+            "Accept": "application/json"
+        ]
+        
+        Alamofire.request("https://init-api.elzup.com/v1/categories", headers:headers)
+            .responseJSON { response in
+                guard let object = response.result.value else {
+                    return
+                }
+                let json = JSON(object)
+                self.categorys.removeAll()
+                json.forEach { (_, json) in
+                    self.categorys.append(json)
+                }
+                self.tableView.reloadData()
+        }
+    }
+}
 
-    // MARK: - Table view data source
+// MARK: UITableViewDataSource method
 
+extension MissionCategoryTableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return categorys.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+        guard let categoryCell = cell as? MissionCategoryTableViewCell else {
+            return cell
+        }
+        categoryCell.categoryLabel.text = categorys[indexPath.row]["name"].stringValue
+        return categoryCell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "MissionListTableViewController", bundle: nil)
+        let missionListController = storyboard.instantiateInitialViewController()
+        guard let secondViewController = missionListController as? MissionListTableViewController else {
+            return
+        }
+        //ここでListViewにデータを移す処理を書く
+//        let array = showOnlyIncompleted ? incompletedMissions : missions
+//        let mission = array[indexPath.row]
+//        secondViewController.title = "詳細"
+//        secondViewController.mission = mission
+        navigationController?.pushViewController(secondViewController, animated: true)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
