4f3b6cd3028d3323f5545373d31057b4fa0d32b9
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2016-12-30 16:22:25 +0900

    MissionCategoryTableViewCellを紐付けた

---
+++ init/Views/MissionCategoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MissionCategoryTableViewCell.swift
+//  init
+//
+//  Created by Atsuo Yonehara on 2016/12/30.
+//  Copyright © 2016年 Atsuo. All rights reserved.
+//
+
+import UIKit
+
+class MissionCategoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
