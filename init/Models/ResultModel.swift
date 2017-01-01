76ff1ce64b24d0afeafc2c6e118337c9be507c66
elzup <guild0105@gmail.com>
2017-01-02 01:22:30 +0900

    Create Result's'Model

--- init/Models/ResultModel.swift
+++ init/Models/ResultModel.swift
@@ -21,3 +21,20 @@ class ResultModel<T: JsonInitializable> {
     }
 }
 
+
+// TODO: 統一したい
+class ResultsModel<T: JsonInitializable> {
+    var data: [T]?
+    var error: ErrorModel?
+    required init(json: JSON) {
+        if json["error"].exists() {
+            self.error = ErrorModel(json: json)
+            return
+        }
+        self.data = json.arrayValue.map { T(json: $0) }
+    }
+
+    func isErorr() -> Bool {
+        return self.error != nil
+    }
+}
