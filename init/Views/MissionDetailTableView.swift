238c64efdecc6b3899e9c81adb972907408cda0f
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-01-28 21:09:47 +0900

    MissionDetailのtableViewを結びつけた

---
+++ init/Views/MissionDetailTableView.swift
@@ -0,0 +1,21 @@
+//
+//  MissionDetailTableView.swift
+//  init
+//
+//  Created by Atsuo Yonehara on 2017/01/28.
+//  Copyright © 2017年 Atsuo. All rights reserved.
+//
+
+import UIKit
+
+class MissionDetailTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
